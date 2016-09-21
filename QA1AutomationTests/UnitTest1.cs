3e3a80dd84abd12529a9b769786ea99b3da6b281
AgentGibbs <christian.gibbs@stgconsulting.com>
2016-09-21 16:00:54 -0600

    Test files

---
+++ QA1AutomationTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QAAutomation1;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace QA1AutomationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver drv;
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //ChromeOptions co = new ChromeOptions();
+            
+            drv = new ChromeDriver();
+            QAAutomation1.Class1 page = new Class1(drv);
+            String title = drv.Title;
+            Console.Write(title);
+            Assert.IsTrue(page.GotoPage("http://www.skiutah.com","Ski Utah-Ski Utah"));
+        }
+    }
+}
