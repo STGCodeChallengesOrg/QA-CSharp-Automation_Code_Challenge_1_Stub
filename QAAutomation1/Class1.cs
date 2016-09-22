90fab396413df13b6c9ffc476c37077b89877aa0
AgentGibbs <christian.gibbs@stgconsulting.com>
2016-09-22 11:08:20 -0600

    Removed incorrect files and uploaded correct files.

---
+++ QAAutomation1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace QAAutomation1
+{
+
+    public class Class1
+    {
+        public IWebDriver driver;
+        public Class1(IWebDriver driverArg) {
+            driver = driverArg;
+            
+        }
+
+
+        public bool GotoPage(String url, String validationString)
+        {
+            driver.Url = url;
+            driver.Navigate();
+            String title = driver.Title;
+            Console.Write(title);
+            //            return title == validationString.ToLower();
+            return title.Contains(validationString);
+
+        }
+
+    }
+}
